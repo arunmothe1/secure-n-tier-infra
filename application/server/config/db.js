057a88fd24095d4066e3fcee138e7c0016a74415
arun <mothearun8443@gmail.com>
2026-08-31 19:12:54 +0530

    all set

--- application/server/config/db.js
+++ application/server/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-// const mongoURI = 'mongodb+srv://crudapp:crudapp@cluster0.iydyt0r.mongodb.net/?retryWrites=true&w=majority';
+// const mongoURI = 'mongodb+srv://crudapp:crudapp@...';
 const mongoURI =
   process.env.MONGO_URI || 'mongodb://localhost:27017/crud';
 
